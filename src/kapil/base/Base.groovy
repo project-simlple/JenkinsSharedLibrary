35b2b650304587aca2378d9921c82cd325053c22
project-simlple <136218875+project-simlple@users.noreply.github.com>
2023-06-20 16:25:18 +0530

    Create Base.groovy

---
+++ src/kapil/base/Base.groovy
@@ -0,0 +1,39 @@
+package kapil.base
+
+import java.util.UUID
+
+
+class Base implements Serializable {
+    protected UUID id;
+    protected def script; //WorkflowScript object
+    protected def jenkins; // hudson.model.Hudson object
+    protected String name;
+
+    Base(script, jenkins) {   // jenkins : class jenkins.model.Jenkins (it is a singleton class)
+        this.id = UUID.randomUUID();
+        this.script = script;
+        this.jenkins = jenkins.get()
+        this.name = ""
+    }
+    public UUID getId() {
+        return this.@id;
+    }
+    public def getScript() {
+        return this.@script;
+    }
+    public def getJenkins() {
+        return this.@jenkins;
+    }
+    public String getName() {
+        return this.@name
+    }
+    public void setScript(script) {
+        this.@script = script;
+    }
+    public void setJenkins(jenkins) {
+        this.@jenkins = jenkins.get()
+    }
+    public void setName(name) {
+        this.@name = name
+    }
+}
